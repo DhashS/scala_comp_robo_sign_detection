a775d00f36bb5bd2cb8af6ad3e02f7ba67b961eb
Dhash S <dhash@outlook.com>
2017-03-11 22:29:21 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "cv-test-drive"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+scalacOptions := Seq("-unchecked", "-deprecation")
+
+libraryDependencies ++= Seq()
---
+++ src/main/scala-2.12/main.scala
@@ -0,0 +1,11 @@
+import org.opencv.core._
+import org.opencv.utils
+/**
+  * Created by dhash on 3/11/17.
+  */
+object main extends App {
+  override def main(args: Array[String]): Unit = {
+    println(Core.NATIVE_LIBRARY_NAME)
+  }
+}
+import org.opencv.core.Algorithm
